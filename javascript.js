f9d37ee1e3083d79368bb9f8e43d2a59c90fe999
16bitsPixel <brandonllanes16@gmail.com>
2023-08-04 20:39:43 -0700

    added eraser tool

--- javascript.js
+++ javascript.js
@@ -1,18 +1,20 @@
-// create a grid of 16 x 16
-let grid = document.querySelector(".grid");
-grid.style.gridTemplateColumns = "repeat(16, 1fr)";
-grid.style.gridTemplateRows = "repeat(16, 1fr)";
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement("div");
-    square.classList.add("pixel");
-    //square.style.border = "1px dashed black";
+// create a grid function
+function createGrid(size) {
+    let grid = document.querySelector(".grid");
+    grid.style.gridTemplateColumns = "repeat(" + size + ", 1fr)";
+    grid.style.gridTemplateRows = "repeat(" + size + ", 1fr)";
+    for (let i = 0; i < size * size; i++) {
+        let square = document.createElement("div");
+        square.classList.add("pixel");
+        //square.style.border = "1px dashed black";
 
-    // if hover over a square, change color
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = colorValue;
-    });
-    
-    grid.append(square);
+        // if hover over a square, change color
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = colorValue;
+        });
+        
+        grid.append(square);
+    }
 }
 
 // color picker
@@ -21,7 +23,9 @@ let color = document.querySelector(".colors");
 let colorValue = "black";
 colorPicker.addEventListener("change", () => {
     color.style.backgroundColor = colorPicker.value;
-    colorValue = colorPicker.value;
+    if (!eraserBtn.checked) {
+        colorValue = colorPicker.value;
+    }
 });
 
 // clear grid button
@@ -32,3 +36,29 @@ function clearGrid() {
     let pixels = Array.from(document.querySelectorAll(".pixel"));
     pixels.forEach(pixel => pixel.style.backgroundColor = "white");
 }
+
+// slider
+let slider = document.querySelector(".slider");
+let sliderText = document.querySelector(".gridSizeText");
+slider.addEventListener("input", () => {
+    clearGrid();
+    createGrid(slider.value);
+    sliderText.textContent = slider.value + " x " + slider.value;
+});
+
+// eraser
+let eraserBtn = document.querySelector(".eraserButton");
+eraserBtn.addEventListener("change", () => {
+    if (eraserBtn.checked) {
+        document.querySelector(".eraser").style.opacity = 1;
+        document.querySelector(".eraser").style.transform = "scale(1.1)";
+        colorValue = "white";
+    }
+    else {
+        document.querySelector(".eraser").style.opacity = 0.8;
+        document.querySelector(".eraser").style.transform = "scale(1)";
+        colorValue = colorPicker.value;
+    }
+});
+
+createGrid(16);
